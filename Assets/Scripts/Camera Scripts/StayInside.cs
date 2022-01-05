cd0107642bf4470c5d73c37015b96f60992e30dd
Jayesh Betala <jayesh.betala7@gmail.com>
2022-01-05 01:17:36 +0000

    Interior

--- Assets/Scripts/Camera Scripts/StayInside.cs
+++ Assets/Scripts/Camera Scripts/StayInside.cs
@@ -8,7 +8,7 @@ public class StayInside : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(Mathf.Clamp(transform.position.x, -0.1f, 100f), 
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, -5f, 7f), 
             transform.position.y, 
             transform.position.z);
     }
